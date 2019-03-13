0941b417e0476290a86edce399f4ea87f343d233
Gilfanov <rgilfanov@fix.ru>
2019-03-13 12:26:39 +0200

    add exchange type support

--- mq_consumer/connectors.py
+++ mq_consumer/connectors.py
@@ -6,8 +6,12 @@ import pika.exceptions
 
 
 class Connector:
-    DELAY_TYPE = 'x-delayed-message'
     DIRECT_TYPE = 'direct'
+    FANOUT_TYPE  = 'fanout'
+    TOPIC_TYPE = 'topic'
+    HEADERS_TYPE = 'headers'
+
+    DELAY_TYPE = 'x-delayed-message'
 
     def __init__(self,
                  connection_parameters: pika.ConnectionParameters,
@@ -15,9 +19,11 @@ class Connector:
                  queue: str,
                  routing_key: str = None,
                  prefetch_count: int = 1,
+                 exchange_type: str = DIRECT_TYPE,
                  use_delay: bool = False):
         self.connection_parameters = connection_parameters
         self.exchange = exchange
+        self.exchange_type = exchange_type
         self.queue = queue
         self.routing_key = routing_key or queue
         self.prefetch_count = prefetch_count
@@ -25,21 +31,19 @@ class Connector:
         self.channel = None
         self.declared_queue = None
         self.use_delay = use_delay
-        if self.use_delay:
-            self.exchange_type = self.DELAY_TYPE
-        else:
-            self.exchange_type = self.DIRECT_TYPE
 
     def create_connection(self):
         self.connection = pika.BlockingConnection(self.connection_parameters)
         self.channel = self.connection.channel()
         self.declared_queue = self.channel.queue_declare(queue=self.queue, durable=True)
         if self.use_delay:
-            arguments = {"x-delayed-type": self.DIRECT_TYPE}
+            exchange_type = self.DELAY_TYPE
+            arguments = {"x-delayed-type": self.exchange_type}
         else:
+            exchange_type = self.exchange_type
             arguments = None
         self.channel.exchange_declare(
-            exchange=self.exchange, exchange_type=self.exchange_type, durable=True, arguments=arguments
+            exchange=self.exchange, exchange_type=exchange_type, durable=True, arguments=arguments
         )
         self.channel.queue_bind(self.queue, self.exchange, routing_key=self.routing_key)
         if self.prefetch_count:
