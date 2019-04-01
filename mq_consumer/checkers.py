f6a1782927dd46821e91375e8136b46faad1db81
rgilfanov <rgilfanov@fix.ru>
2019-04-01 23:41:29 +0300

    add passive mode

--- mq_consumer/checkers.py
+++ mq_consumer/checkers.py
@@ -2,13 +2,14 @@ from mq_consumer.connectors import Connector
 
 
 class MQChecker:
-    def __init__(self, connector: Connector):
+    def __init__(self, connector: Connector, passive=True):
         self.connector = connector
+        self.passive = passive
 
     def get_message_count(self):
         self.connector.create_connection(declare=False)
         try:
-            msg_count = self.connector.channel.queue_declare(queue=self.connector.queue, passive=True).method.message_count
+            msg_count = self.connector.channel.queue_declare(queue=self.connector.queue, passive=self.passive).method.message_count
         finally:
             self.connector.close()
         return msg_count
